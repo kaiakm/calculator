30d7fbda316afe0db5f6ce15d12cb7a53e32b45e
kaia <89624646+kaiakm@users.noreply.github.com>
2023-05-03 16:53:06 -0500

    phasing out the operate for now

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-let firstNumber = '';
-let secondNumber = '';
-let operator = '';
-let displayValue = '0';
+const numberBtn = document.querySelectorAll('.number');
+const operateBtn = document.querySelectorAll('.operator');
+const equalsBtn = document.querySelector('.equals');
+const deleteBtn = document.querySelector('.delete');
+const clearBtn = document.querySelector('.clear');
+const firstNumber = document.querySelector('.first-number');
+const secondNumber = document.querySelector('.second-number');
 
-function add(a, b) {
-    return a + b;
-};
+// function add(a, b) {
+//     return a + b;
+// };
 
-function subtract(a, b) {
-    return a - b;
-};
+// function subtract(a, b) {
+//     return a - b;
+// };
 
-function multiply(a, b) {
-    return a * b;
-};
+// function multiply(a, b) {
+//     return a * b;
+// };
 
-function divide(a, b) {
-    if (b === 0) {
-        return 'error';
-    } else {
-        return a / b;
-    };
-};
+// function divide(a, b) {
+//     if (b === 0) {
+//         return 'error';
+//     } else {
+//         return a / b;
+//     };
+// };
 
-function operate(operator, a, b) {
-    if (operator === '+') {
-        return add(a, b);
-    } else if (operator === '-') {
-        return subtract(a, b);
-    } else if (operator === 'x') {
-        return multiply(a, b) ;
-    } else if (operator === '/') {
-        return divide(a, b);
-    };
-};
+// function operate(operator, a, b) {
+//     if (operator === '+') {
+//         return add(a, b);
+//     } else if (operator === '-') {
+//         return subtract(a, b);
+//     } else if (operator === 'x') {
+//         return multiply(a, b) ;
+//     } else if (operator === '/') {
+//         return divide(a, b);
+//     };
+// };
 
-const display = document.querySelector('.display');
+// const display = document.querySelector('.display');
