25ed007c72abb3ecdb451e5205cd2c7644b401ab
kaia <89624646+kaiakm@users.noreply.github.com>
2023-05-03 08:08:31 -0500

    numbers are now populating on display

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let firstNumber = '';
 let secondNumber = '';
 let operator = '';
+let displayValue = '0';
 
 function add(a, b) {
     return a + b;
@@ -33,3 +34,41 @@ function operate(operator, a, b) {
         return divide(a, b);
     };
 };
+
+const display = document.querySelector('.display');
+
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.classList.contains('clear')) {
+            displayValue = '0';
+        } else if (button.classList.contains('delete')) {
+            displayValue = displayValue.slice(0, -1);
+            if (displayValue === '') {
+                displayValue = '0';
+            };
+        } else if (button.classList.contains('equal')) {
+            if (operator && secondNumber) {
+                displayValue = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber)).toString();
+                firstNumber = displayValue;
+                secondNumber = '';
+                operator = '';
+            };
+        } else if (button.classList.contains('add') || button.classList.contains('subtract') || button.classList.contains('multiply') || button.classList.contains('divide')) {
+            if (firstNumber && operator && secondNumber) {
+                displayValue = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber)).toString();
+                firstNumber = displayValue;
+                secondNumber = '';
+            };
+            operator = button.textContent;
+        } else {
+            if (operator) {
+                secondNumber += button.textContent;
+                displayValue = secondNumber;
+            } else {
+                firstNumber += button.textContent;
+                displayValue = firstNumber;
+            };
+        };
+        display.innerHTML = displayValue;
+    });
+});
