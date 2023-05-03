a4031b05a83b822ad9550da779b19917f1054822
kaia <89624646+kaiakm@users.noreply.github.com>
2023-05-03 16:14:49 -0500

    code became bloated and confusing. will start over

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function operate(operator, a, b) {
         return add(a, b);
     } else if (operator === '-') {
         return subtract(a, b);
-    } else if (operator === '*') {
+    } else if (operator === 'x') {
         return multiply(a, b) ;
     } else if (operator === '/') {
         return divide(a, b);
@@ -36,39 +36,3 @@ function operate(operator, a, b) {
 };
 
 const display = document.querySelector('.display');
-
-document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', () => {
-        if (button.classList.contains('clear')) {
-            displayValue = '0';
-        } else if (button.classList.contains('delete')) {
-            displayValue = displayValue.slice(0, -1);
-            if (displayValue === '') {
-                displayValue = '0';
-            };
-        } else if (button.classList.contains('equal')) {
-            if (operator && secondNumber) {
-                displayValue = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber)).toString();
-                firstNumber = displayValue;
-                secondNumber = '';
-                operator = '';
-            };
-        } else if (button.classList.contains('add') || button.classList.contains('subtract') || button.classList.contains('multiply') || button.classList.contains('divide')) {
-            if (firstNumber && operator && secondNumber) {
-                displayValue = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber)).toString();
-                firstNumber = displayValue;
-                secondNumber = '';
-            };
-            operator = button.textContent;
-        } else {
-            if (operator) {
-                secondNumber += button.textContent;
-                displayValue = secondNumber;
-            } else {
-                firstNumber += button.textContent;
-                displayValue = firstNumber;
-            };
-        };
-        display.innerHTML = displayValue;
-    });
-});
