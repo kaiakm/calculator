72c38f092f1e5dc22f906a83563109ff2a41939e
kaia <89624646+kaiakm@users.noreply.github.com>
2023-05-03 07:30:15 -0500

    added operator functions

---
+++ script.js
@@ -0,0 +1,35 @@
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    if (b === 0) {
+        return 'error';
+    } else {
+        return a / b;
+    };
+};
+
+function operate(operator, a, b) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b) ;
+    } else if (operator === '/') {
+        return divide(a, b);
+    };
+};
