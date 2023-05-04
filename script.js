a77aeb69a51b4b225562b0664cc4c39ba274c69c
kaia <89624646+kaiakm@users.noreply.github.com>
2023-05-03 19:14:54 -0500

    i am stuck

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
-const numberBtn = document.querySelectorAll('.number');
-const operateBtn = document.querySelectorAll('.operator');
-const equalsBtn = document.querySelector('.equals');
-const deleteBtn = document.querySelector('.delete');
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let displayValue = '';
+const display = document.querySelector('.display');
+const numberBtns = document.querySelectorAll('.number');
+const operateBtns = document.querySelectorAll('.operator');
 const clearBtn = document.querySelector('.clear');
-const firstNumber = document.querySelector('.first-number');
-const secondNumber = document.querySelector('.second-number');
 
-class Calculator {
-    constructor(firstNumber, secondNumber) {
-        this.firstNumber = firstNumber;
-        this.secondNumber = secondNumber;
-        this.clear();
-    }
-}
-
-clear(){
-
+function add(a, b) {
+    return a + b;
 };
 
-// function add(a, b) {
-//     return a + b;
-// };
+function subtract(a, b) {
+    return a - b;
+};
 
-// function subtract(a, b) {
-//     return a - b;
-// };
+function multiply(a, b) {
+    return a * b;
+};
 
-// function multiply(a, b) {
-//     return a * b;
-// };
+function divide(a, b) {
+    if (b === 0) {
+        return 'error';
+    } else {
+        return a / b;
+    };
+};
 
-// function divide(a, b) {
-//     if (b === 0) {
-//         return 'error';
-//     } else {
-//         return a / b;
-//     };
-// };
+function operate(operator, a, b) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === 'x') {
+        return multiply(a, b) ;
+    } else if (operator === '/') {
+        return divide(a, b);
+    };
+};
 
-// function operate(operator, a, b) {
-//     if (operator === '+') {
-//         return add(a, b);
-//     } else if (operator === '-') {
-//         return subtract(a, b);
-//     } else if (operator === 'x') {
-//         return multiply(a, b) ;
-//     } else if (operator === '/') {
-//         return divide(a, b);
-//     };
-// };
+numberBtns.forEach(function(numberBtn) {
+    numberBtn.addEventListener('click', function() {
+        const number = numberBtn.textContent;
+        displayValue += number;
+        display.textContent = displayValue;
+    });
+});
 
-// const display = document.querySelector('.display');
+function updateDisplay(number) {
+    displayValue += number;
+    document.querySelector('.display').textContent = displayValue;
+};
